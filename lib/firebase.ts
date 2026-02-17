fba1b8b952aeadf16dcec0184668d1f2c7141219
alopopau-eng <alopopau@gmail.com>
2026-02-17 09:09:24 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,14 +5,14 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDABw1C30Hscha9m--8OgOHgOe35vfgfvE",
-  authDomain: "abds-dc4aa.firebaseapp.com",
-  databaseURL: "https://abds-dc4aa-default-rtdb.firebaseio.com",
-  projectId: "abds-dc4aa",
-  storageBucket: "abds-dc4aa.firebasestorage.app",
-  messagingSenderId: "1076311425985",
-  appId: "1:1076311425985:web:01836a0f2a968f86c5a540",
-  measurementId: "G-LGTNFCBFGJ"
+  apiKey: "AIzaSyC5q2ReYxL2qNdx6NbYfk8CjsHgoITFNtI",
+  authDomain: "zainsps-92746.firebaseapp.com",
+  databaseURL: "https://zainsps-92746-default-rtdb.firebaseio.com",
+  projectId: "zainsps-92746",
+  storageBucket: "zainsps-92746.firebasestorage.app",
+  messagingSenderId: "1048830927722",
+  appId: "1:1048830927722:web:d723eb77b0cc14f4587b13",
+  measurementId: "G-7HPMLXNQGK"
 }
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
